8e1b2ba1d59de6914fe35082ae6ca4c7e7f9b6b6
darshanahs1473 <azh2kor@bosch.com>
2025-09-22 23:32:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,34 @@
+
 import pytest
 from string_calculator import StringCalculator
 
 
 @pytest.fixture
 def calc():
-return StringCalculator()
+    return StringCalculator()
 
 
-@pytest.mark.parametrize("input_str, expected", [
-("", 0),
-("1", 1),
-("1,2", 3),
-("1,2,3,4", 10),
-("1\n2,3", 6),
-("//;\n1;2", 3),
-("2,1001", 2),
-("//[***]\n1***2***3", 6),
-])
-def test_valid_cases(calc, input_str, expected):
-assert calc.add(input_str) == expected
+@pytest.mark.parametrize(
+    "expr, expected",
+    [
+        ("", 0),
+        ("1", 1),
+        ("1,2", 3),
+        ("1,2,3,4", 10),
+        ("1\n2,3", 6),
+        ("//;\n1;2", 3),
+        ("2,1001", 2),
+        ("//[***]\n1***2***3", 6),
+    ],
+)
+def test_expressions(calc, expr, expected):
+    assert calc.add(expr) == expected
 
 
-
-
-def test_negatives_raise(calc):
-with pytest.raises(ValueError) as excinfo:
-calc.add("1,-2,-3,4")
-msg = str(excinfo.value)
-assert "negatives not allowed" in msg
-assert "-2" in msg
-assert "-3" in msg
-
-if __name__ == "__main__":
-unittest.main()
+def test_negatives(calc):
+    with pytest.raises(ValueError) as e:
+        calc.add("1,-2,-3")
+    msg = str(e.value)
+    assert "negatives not allowed" in msg
+    assert "-2" in msg
+    assert "-3" in msg
